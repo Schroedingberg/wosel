9fe14e6d6f2a1366a116b04c4e03ea0890f4a000
Schroedingberg <aaronrebmann@gmail.com>
2016-04-30 18:33:02 +1000

    Added the source file with the usual default structure

---
+++ wos.el
@@ -0,0 +1,14 @@
+;;; package: --- Wosel --- An Emacs Web of Science browser
+;;; Commentary:
+;;; Author: Aaron Rebmann
+
+
+
+
+
+
+;;; Code:
+;(require 'helm-core)
+;(require 'ebib)
+(provide 'wos)
+;;; wos.el ends here
